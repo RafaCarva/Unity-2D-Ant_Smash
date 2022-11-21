3be1759564fb25cd3db1a8d89f4098225e68c2f7
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2022-11-21 19:26:36 -0300

    Animação da velocidade dos inimigos

--- Assets/_Game/Scripts/Enemy.cs
+++ Assets/_Game/Scripts/Enemy.cs
@@ -7,21 +7,29 @@ public class Enemy : MonoBehaviour
 
     public float speed;
 
+    private Animator myAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        AnimationSpeed();
     }
 
     private void Movement()
     {
         transform.Translate(Vector2.down * (speed * Time.deltaTime));
+    }
+
+    private void AnimationSpeed()
+    {
+        myAnimator.SetFloat("Speed", speed);
     }
 
 }
