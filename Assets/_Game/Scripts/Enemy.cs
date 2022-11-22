606e8987520caa8350d4d6da94785e0ca49d032d
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2022-11-21 23:04:56 -0300

    Destruição de inimigos.

--- Assets/_Game/Scripts/Enemy.cs
+++ Assets/_Game/Scripts/Enemy.cs
@@ -9,10 +9,14 @@ public class Enemy : MonoBehaviour
 
     private Animator myAnimator;
 
+    [SerializeField] private GameObject[] sprites;
+
     // Start is called before the first frame update
     void Start()
     {
         myAnimator = GetComponent<Animator>();
+        sprites[0] = this.transform.GetChild(0).gameObject; // Esse é o filho que tem o sprite da formiga viva.
+        sprites[1] = this.transform.GetChild(1).gameObject;
     }
 
     // Update is called once per frame
@@ -30,6 +34,14 @@ public class Enemy : MonoBehaviour
     private void AnimationSpeed()
     {
         myAnimator.SetFloat("Speed", speed);
+    }
+
+    public void Dead()
+    {
+        speed = 0f;
+        sprites[0].gameObject.SetActive(false);
+        sprites[1].gameObject.SetActive(true);
+        Destroy(this.gameObject, Random.Range(2.5f, 5f));
     }
 
 }
---
+++ Assets/_Game/Scripts/TapDetected.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapDetected : MonoBehaviour
+{
+    private bool tapControl;
+    private Enemy enemy;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectTap();
+    }
+
+    private void DetectTap()
+    {
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint((Input.GetTouch(0).position)), Vector2.zero);
+
+            if(hit.collider != null)
+            {
+                TapObject(hit);
+                tapControl = false;
+            }
+        }
+    }
+
+    private void TapObject(RaycastHit2D hit)
+    {
+        if(hit.collider.gameObject.CompareTag("Enemy") && !tapControl)
+        {
+            tapControl = true;
+            hit.collider.gameObject.GetComponent<Enemy>().Dead();
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
