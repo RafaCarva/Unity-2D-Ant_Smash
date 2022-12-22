1bb6d8494b5383f295d2e087260aba5c772a3a04
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2022-12-21 22:40:48 -0300

    Ants spawner

---
+++ Assets/_Game/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private float minX, maxX, nextSpawn ;
+    [SerializeField] private float minDistance, maxDistance, spawnTime;
+    [SerializeField] private GameObject[] enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSpawn = Time.time;
+        SetMinAndMaxX();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Spawn();
+    }
+
+    private void SetMinAndMaxX()
+    {
+        // Pegar a largura da tela
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.safeArea.width, 0f, 0f));
+        minX = -bounds.x + minDistance;
+        maxX = bounds.x + maxDistance;
+    }
+
+    private void Spawn()
+    {
+        if (Time.time > nextSpawn)
+        {
+            Vector2 position = new Vector2(Random.Range(minX, maxX), transform.position.y);
+            Instantiate(enemies[Random.Range(0, enemies.Length)], new Vector2(position.x, position.y), Quaternion.Euler(0f, 0f, 0f));
+            nextSpawn = Time.time + spawnTime;
+        }
+    }
+}
