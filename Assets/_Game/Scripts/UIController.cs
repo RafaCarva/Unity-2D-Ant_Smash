c668ca2fdbe259ba422fd7f44205557887158ee2
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2022-12-30 19:48:21 -0300

    Pause do game

--- Assets/_Game/Scripts/UIController.cs
+++ Assets/_Game/Scripts/UIController.cs
@@ -8,13 +8,15 @@ public class UIController : MonoBehaviour
 {
 
     public TMP_Text txtScore;
-
     public Image[] imageLifes;
+
+    public GameObject panelGame, panelPause;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        panelGame.gameObject.SetActive(true);
+        panelPause.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,6 +28,27 @@ public class UIController : MonoBehaviour
     public void UpdateScore (int score)
     {
         txtScore.text = score.ToString();
+    }
+
+    public void ButtonPause()
+    {
+        Time.timeScale = 0f;
+        panelGame.gameObject.SetActive(false);
+        panelPause.gameObject.SetActive(true);
+    }
+
+    public void ButtonResume()
+    {
+        Time.timeScale = 1f;
+        panelGame.gameObject.SetActive(true);
+        panelPause.gameObject.SetActive(false);
+    }
+
+    public void ButtonBackMainMenu()
+    {
+        Time.timeScale = 1f;
+        panelGame.gameObject.SetActive(false);
+        panelPause.gameObject.SetActive(false);
     }
 
 }
